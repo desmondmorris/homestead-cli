4e3203757f260bacf95c5c3efcd8f4dfabacac1d
Desmond Morris <hi@desmondmorris.com>
2014-05-21 00:21:37 -0400

    Adds HomesteadCommand for base methods and reusability

---
+++ src/Homestead/Commands/HomesteadCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Homestead\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class HomesteadCommand extends Command
+{
+    protected static $install_dir = ".homestead";
+
+    protected static function getInstallPath() {
+        return $_SERVER['HOME'] . DIRECTORY_SEPARATOR . self::$install_dir;
+    }
+
+    protected static function has_git()
+    {
+      exec('which git', $output);
+      $git = file_exists($line = trim(current($output))) ? $line : 'git';
+      unset($output);
+      exec($git . ' --version', $output);
+      preg_match('#^(git version)#', current($output), $matches);
+      return ! empty($matches[0]) ? $git : false;
+    }
+}
--- src/Homestead/Commands/UpdateCommand.php
+++ src/Homestead/Commands/UpdateCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class UpdateCommand extends Command
+class UpdateCommand extends HomesteadCommand
 {
     protected function configure()
     {
@@ -19,8 +19,7 @@ class UpdateCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $home = $_SERVER['HOME'];
-        $homestead_path = $home . '/' . '.homestead';
+        $homestead_path = self::getInstallPath();
 
         $output->writeln("Updating Homestead");
 
@@ -42,15 +41,5 @@ class UpdateCommand extends Command
 
         }
 
-    }
-
-    private static function has_git()
-    {
-      exec('which git', $output);
-      $git = file_exists($line = trim(current($output))) ? $line : 'git';
-      unset($output);
-      exec($git . ' --version', $output);
-      preg_match('#^(git version)#', current($output), $matches);
-      return ! empty($matches[0]) ? $git : false;
     }
 }
